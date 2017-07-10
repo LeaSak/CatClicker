a11b3fd10bf969d0a398f0e12ed1e23c730569c7
Lea-Ann_Sakmann <leaann.sakmann@gmail.com>
2017-07-10 10:26:19 +0200

    Update catclicker app to meet project revision specs 2.

--- app.js
+++ app.js
@@ -1,69 +1,119 @@
-// var columnLeft = document.getElementById('left');
-// var columnRight = document.getElementById('right');
-// var felineTarget = document.getElementById('feline');
-// var totalClicks = document.getElementById('clickCounter')
-// var counter = 0;
+var catLinks = document.getElementById('cat-list');
+var catBox = document.getElementById('cat-box');
 
-// totalClicks.innerHTML = '<p>Clicks: ' + counter + '</p>';
-
-// felineTarget.addEventListener("click", function(){
-//     //console.log("You clicked.");
-//     counter ++;
-//     totalClicks.innerHTML = '<p>Clicks: ' + counter + '</p>'
-
-// }, false);
-
-var counter = 0;
-// var counterString = '<p class="tally">Clicks: %data%</p>';
-
-var Cat = function(name, sprite, column){
+var Cat = function(name, sprite) {
     this.name = name;
     this.sprite = sprite;
-    this.column = column;
     this.counter = 0;
 }
 
-Cat.prototype.renderTitle = function(){
+Cat.prototype.renderName = function() {
+    this.linkID = this.name + "Link";
+    this.capitalizeFirstLetter();
+    this.catLink = document.createElement('li');
+    this.catLink.innerHTML = '<a href="" class="profile-link" id="' + this.linkID + '">' + this.title + '</a>';
+    catLinks.appendChild(this.catLink);
+}
+
+Cat.prototype.capitalizeFirstLetter = function() {
     var lowercaseString = this.name.slice(1);
     var firstLetter = this.name.charAt(0).toUpperCase();
-    this.title = firstLetter + lowercaseString;
-    $(this.column).append('<h2 class="title">' + this.title + '</h2>');
+    return this.title = firstLetter + lowercaseString;
 }
 
-Cat.prototype.renderImage = function(){
-    $(this.column).append('<img class="cat"' +' src="' + this.sprite + '"' + ' alt="cat">');
-    $("img:last").attr("id", this.name);
+Cat.prototype.renderTitle = function() {
+    this.capitalizeFirstLetter();
+    var catTitle = document.createElement('h2');
+    catTitle.className = 'title';
+    catTitle.innerHTML = 'Meet ' + this.title;
+    catBox.appendChild(catTitle);
 }
 
-Cat.prototype.renderClicks = function(){
+Cat.prototype.renderImage = function() {
+    this.imageID = this.name + 'Image';
+    var catImage = document.createElement('img');
+    catImage.setAttribute('alt', 'cat');
+    catImage.className = 'cat';
+    catImage.id = this.imageID;
+    catImage.src = this.sprite;
+    catBox.appendChild(catImage);
+}
+
+Cat.prototype.renderClicks = function() {
     this.counterId = this.name + "Counter";
-    $(this.column).append('<p class="tally">Clicks: ' + '<span class="click-counter">' + this.counter + '</span></p>');
-    $('.click-counter:last').attr("id", this.counterId);
+    var catCounter = document.createElement('p');
+    catCounter.className = 'tally';
+    catCounter.innerHTML = 'Clicks: ' + '<span id="' + this.counterId + '">' + this.counter + '</span>'
+    catBox.appendChild(catCounter);
 }
 
-Cat.prototype.updateClicks = function(){
-    this.counter ++;
-    $('#' + this.counterId).html(this.counter);
+Cat.prototype.renderCatProfile = function() {
+    this.renderTitle();
+    this.renderImage();
+    this.renderClicks();
 }
 
-var fluffy = new Cat('fluffy', 'images/cat.jpg', '#left');
+Cat.prototype.updateClicks = function() {
+    this.counter++;
+    this.counterData = document.getElementById(this.counterId);
+    this.counterData.innerHTML = this.counter;
+}
+
+// Create cat objects
+// Photo by Miguel Angel Ruíz Sánchez on Unsplash
+var fluffy = new Cat('fluffy', 'images/cat.jpg');
 // Photo by 西爾維亞 on Unsplash
-var ginger = new Cat('ginger', 'images/ginger.jpg', '#right');
+var ginger = new Cat('ginger', 'images/ginger.jpg');
 
+// Create cat array
 var allCats = [fluffy, ginger];
+//var selectedCat = 0;
 
-allCats.forEach(function(cat){
-    cat.renderTitle();
-    cat.renderImage();
-    cat.renderClicks();
+
+
+for (var i = 0; i < allCats.length; i++) {
+    var cat = allCats[i];
+    cat.renderName();
+    // cat.catLink.addEventListener('click', (function(catCopy, x){
+    //     return function(e){
+    //         e.preventDefault();
+    //         catBox.innerHTML = '';
+    //         catCopy.renderCatProfile();
+    //         selectedCat = x;
+    //         console.log(allCats[x].name);
+    //     };
+    // })(cat,i));
+    // catBox.addEventListener('click', function(e){
+    //     //allCats[selectedCat].updateClicks();
+    //     e.preventDefault();
+    //     allCats[selectedCat].counter++;
+    //     console.log(allCats[selectedCat].counter);
+    // })
+}
+
+// event.target = 'a'
+// event.currentTarget = catLinks/ the ul
+catLinks.addEventListener('click', function(e) {
+    console.log(e.target.id);
+    //show profile of cat with the id matching the target
+    allCats.forEach(function(cat){
+        if(cat.linkID === e.target.id){
+            catBox.innerHTML = '';
+            cat.renderCatProfile();
+        }
+    })
+    e.preventDefault();
 });
 
+catBox.addEventListener('click', function(e){
+    if(e.target.nodeName === 'IMG'){
+        allCats.forEach(function(cat){
+        if(cat.imageID === e.target.id){
+            cat.updateClicks();
+        }
+    })
 
-// assign click counter to individual cat image with id of cat name
-$("#fluffy").click(function(){
-    fluffy.updateClicks();
+    }
+    e.preventDefault();
 })
 
-$("#ginger").click(function(){
-    ginger.updateClicks();
-})
