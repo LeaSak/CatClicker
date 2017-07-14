e2432897385cd25328332287b7d14e3b93941886
Lea-Ann_Sakmann <leaann.sakmann@gmail.com>
2017-07-14 14:36:33 +0200

    Add Admin section.

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ var Cat = function(name, sprite) {
     this.catLinkID = this.name + 'Link';
     this.imageID = this.name + 'Image';
     this.counterId = this.name + "Counter";
+    this.catBtnId = this.name + "Btn";
+    this.saveBtnId = this.name + "SaveBtn";
 }
 
 // Create cat objects
@@ -26,6 +28,9 @@ var octopus = {
     getCats: function() {
         return model.cats;
     },
+    getCurrentCat: function(cat){
+        return model.cats.indexOf(cat);
+    },
     capitalizeFirstLetter: function(cat) {
         var lowercaseString = cat.name.slice(1);
         var firstLetter = cat.name.charAt(0).toUpperCase();
@@ -37,104 +42,151 @@ var octopus = {
     init: function() {
         catListView.init();
         catProfileView.init();
+        catAdminView.init();
     }
 }
 
 /* ======= Render ======= */
 //Render Cat Name
-//Render Cat Profile and Click Counter
-//TODO: 0ctopus function is repeated...how to fix?
-
 var catListView = {
-    init: function() {
-        this.catLinks = document.getElementById('cat-list');
-        this.cats = octopus.getCats();
-        this.renderList();
-        this.renderProfileOnClick();
-    },
-    renderList: function() {
-        var cats, catListElem, catLink;
-        //get cats
-        cats = octopus.getCats();
-        //create list item for each cat
-        cats.forEach(function(cat) {
-            octopus.capitalizeFirstLetter(cat);
-            catListElem = document.createElement('li');
-            catLink = document.createElement('a');
-            catLink.href = '';
-            catLink.id = cat.catLinkID;
-            catLink.className = 'profile-link';
-            catLink.textContent = cat.title;
-            catListElem.appendChild(catLink);
-            this.catLinks.appendChild(catListElem);
-        }, catListView)
+        init: function() {
+            this.catLinks = document.getElementById('cat-list');
+            this.cats = octopus.getCats();
+            this.renderList();
+            this.renderProfileOnClick();
+        },
+        renderList: function() {
+            var catListElem, catLink;
+            this.catLinks.innerHTML = '';
+            //create list item for each cat
+            this.cats.forEach(function(cat) {
+                octopus.capitalizeFirstLetter(cat);
+                catListElem = document.createElement('li');
+                catLink = document.createElement('a');
+                catLink.href = '';
+                catLink.id = cat.catLinkID;
+                catLink.className = 'profile-link';
+                catLink.textContent = cat.title;
+                catListElem.appendChild(catLink);
+                this.catLinks.appendChild(catListElem);
+            }, catListView)
 
-    },
-    renderProfileOnClick: function() {
-        var cats = octopus.getCats();
-        this.catLinks.addEventListener('click', function(e) {
-            for (var i = 0; i < cats.length; i++) {
-                cat = cats[i];
+        },
+
+        renderProfileOnClick: function() {
+            this.catLinks.addEventListener('click', this.showProfile.bind(this), false)
+        },
+
+        showProfile: function(e) {
+            for (var i = 0; i < this.cats.length; i++) {
+                cat = this.cats[i];
                 if (cat.catLinkID === e.target.id) {
-                    catProfileView.clearProfile();
                     catProfileView.renderTitle();
                     catProfileView.renderImage();
                     catProfileView.renderClicks();
+                    catAdminView.renderAdminButton();
                 }
             }
+            catAdminView.hideCatForm();
             e.preventDefault();
-        })
-    }
-
-};
-
-
-var catProfileView = {
-    init: function() {
-        this.catBox = document.getElementById('cat-box');
-        this.incrementCounterOnClick();
-    },
-    clearProfile: function() {
-        this.catBox.innerHTML = '';
-    },
-    renderTitle: function() {
-        octopus.capitalizeFirstLetter(cat);
-        var catTitle = document.createElement('h2');
-        catTitle.className = 'title';
-        catTitle.textContent = 'Meet ' + cat.title;
-        this.catBox.append(catTitle);
-    },
-    renderImage: function() {
-        var catImage = document.createElement('img');
-        catImage.setAttribute('alt', 'cat');
-        catImage.className = 'cat';
-        catImage.id = cat.imageID;
-        catImage.src = cat.sprite;
-        this.catBox.appendChild(catImage);
-    },
-    renderClicks: function() {
-        var catCounter = document.createElement('p');
-        catCounter.className = 'tally';
-        catCounter.textContent = 'Clicks: ';
-        var counterDataElem = document.createElement('span');
-        counterDataElem.id = cat.counterId;
-        counterDataElem.textContent = cat.counter;
-        catCounter.appendChild(counterDataElem);
-        this.catBox.appendChild(catCounter);
-    },
-    incrementCounterOnClick: function() {
-        var cats = octopus.getCats();
-        this.catBox.addEventListener('click', function(e) {
-            for (var i = 0; i < cats.length; i++) {
-                var cat = cats[i];
-                if (cat.imageID === e.target.id) {
-                    octopus.incrementCounter(cat);
-                    document.getElementById(cat.counterId).textContent = cat.counter;
-                }
             }
-            e.preventDefault();
-        })
-    }
-};
+        };
 
-octopus.init();
+//Render Cat Profile and Click Counter
+        var catProfileView = {
+            init: function() {
+                this.catBox = document.getElementById('cat-box');
+                this.catTitle = document.getElementById('cat-intro');
+                this.catImage = document.getElementById('cat-image');
+                this.catCounter = document.getElementById('cat-counter');
+                this.incrementCounterOnClick();
+            },
+            /*
+             * Render Profile Functions
+             * Title, Image and Total Clicks
+             */
+
+            renderTitle: function() {
+                octopus.capitalizeFirstLetter(cat);
+                this.catTitle.textContent = 'Meet ' + cat.title;
+            },
+            renderImage: function() {
+                this.catImage.id = cat.imageID;
+                this.catImage.src = cat.sprite;
+            },
+            renderClicks: function() {
+                this.catCounter.id = cat.counterId;
+                this.catCounter.textContent = "Clicks: " + cat.counter;
+            },
+            // Update Total Clicks for cat if ID matches
+            incrementCounterOnClick: function() {
+                this.catBox.addEventListener('click', function(e) {
+                    for (var i = 0; i < catListView.cats.length; i++) {
+                        var cat = catListView.cats[i];
+                        if (cat.imageID === e.target.id) {
+                            octopus.incrementCounter(cat);
+                             document.getElementById(cat.counterId).textContent = "Clicks: " + cat.counter;
+                             catAdminView.catClicks.value = cat.counter;
+                        }
+                    }
+                    e.preventDefault();
+                })
+            }
+        };
+
+//Render Cat Admin Section
+        var catAdminView = {
+            init: function(){
+                this.catAdminBox = document.getElementById('admin-box');
+                this.catAdminBtn = document.getElementById('cat-btn');
+                this.catForm = document.getElementById('cat-form');
+                this.catNameFormElem = document.getElementById('cat-name');
+                this.catURLFormElem = document.getElementById('cat-url');
+                this.catClicks = document.getElementById('cat-clicks');
+                this.cancelBtn = document.getElementById('cancel-btn');
+                this.saveBtn = document.getElementById('save-btn');
+                this.renderCatFormOnClick();
+                this.saveForm();
+                this.cancel();
+            },
+            renderAdminButton: function(){
+                this.catAdminBtn.id = cat.catBtnId;
+                this.catAdminBtn.style.display= 'block';
+            },
+            hideCatForm: function(){
+                this.catForm.style.display = 'none';
+            },
+            showCatForm: function(e){
+                for (var i = 0; i < catListView.cats.length; i++){
+                    var cat = catListView.cats[i];
+                    if(cat.catBtnId === e.target.id){
+                        octopus.capitalizeFirstLetter(cat);
+                        this.catNameFormElem.value = cat.title;
+                        this.catURLFormElem.value = cat.sprite;
+                        this.catClicks.value = cat.counter;
+                        this.catForm.style.display = 'block';
+                    }
+                }
+                e.preventDefault();
+            },
+            updateTitle: function(){
+                // get current cat
+                console.log(cat.name);
+                // cat.name = this.catNameFormElem.value.toLowerCase();
+                // cat.catLinkID = cat.name + 'Link';
+                // console.log(cat.name, cat.catLinkID);
+                // catProfileView.renderTitle();
+            },
+            // Form Event Listeners
+            renderCatFormOnClick: function(){
+                this.catAdminBtn.addEventListener('click', this.showCatForm.bind(this), false);
+            },
+            saveForm: function(){
+                this.saveBtn.addEventListener('click', this.updateTitle.bind(this), false);
+            },
+            cancel: function(){
+                this.cancelBtn.addEventListener('click', this.hideCatForm.bind(this), false);
+            }
+        };
+
+        octopus.init();
